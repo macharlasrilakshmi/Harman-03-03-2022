d587da35e95b8e3ea63e9fd958134047a9cbf614
macharlasrilakshmi <101548823+macharlasrilakshmi@users.noreply.github.com>
2022-03-14 20:24:53 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,2 @@
+print(("Employee Name:- "+input("Enter the Name: ")+"\n") + ("Employee Age:- "+input("Enter Age: ")+"\n") +
+      ("Employee Designation:- "+input("Enter Designation: ")+"\n") + ("Employee Salary:- "+input("Enter the Salary: ")))
