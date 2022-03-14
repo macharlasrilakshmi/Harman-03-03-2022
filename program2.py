ec2b9039ae388e9f23491c856dfff90cb44f4959
macharlasrilakshmi <101548823+macharlasrilakshmi@users.noreply.github.com>
2022-03-14 20:23:46 +0530

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,6 @@
+User_input = input("Enter a phrase : ")
+Reverse_string = User_input[::-1]
+if User_input == Reverse_string:
+    print("It is a PALINDROME")
+else:
+    print("It is NOT a palindrome")
